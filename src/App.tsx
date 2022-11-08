229dd12e8ab386d69c16fe95486a7f8aed9e5221
Jaz Singh <jaz@color.com>
2022-11-07 16:12:30 -0800

    Add theme providers to allow us to test overrides

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { createTheme as createThemeV4, ThemeProvider as ThemeProviderV4 } from "@material-ui/core";
+import { createTheme as createThemeV5, ThemeProvider as ThemeProviderV5 } from "@mui/material";
 import { DialogsToTestBodyScrollBug } from "DialogsToTestBodyScrollBug";
 
+const themeV4 = createThemeV4({
+  props: {
+    MuiModal: {
+      // disableScrollLock: true,
+    },
+    MuiDialog: {
+      scroll: 'body',
+      TransitionProps: { role: 'none' } as any,
+    },
+  },
+})
+
+const themeV5 = createThemeV5({
+  components: {
+    MuiModal: {
+      // defaultProps: {
+      //   disableScrollLock: true,
+      // }
+    },
+    MuiDialog: {
+      defaultProps: {
+        scroll: 'body',
+        TransitionProps: { role: 'none' } as any,
+      }
+    },
+  }
+})
+
 export function App() {
-  return <DialogsToTestBodyScrollBug />
+  return (
+    <ThemeProviderV4 theme={themeV4}>
+      <ThemeProviderV5 theme={themeV5}>
+        <DialogsToTestBodyScrollBug />
+      </ThemeProviderV5>
+    </ThemeProviderV4>
+  )
 }
